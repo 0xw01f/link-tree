222822c22d59f8be95062316219d1249434ec8e8
W01F <76235339+0xw01f@users.noreply.github.com>
2023-01-24 22:28:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#titleType').typewrite({
-        speed: 5,
+        speed: 10,
         cursor: '.',
         actions: [
             {type: 'Loading..'},
@@ -14,5 +14,5 @@ $(window).on('load',function(){
 	setTimeout(function(){ // allowing 3 secs to fade out loader
 	$('.page-loader').fadeOut('slow');
     jQuery('.content').css('opacity', '1');
-	},3500);
+	},2200);
 });
